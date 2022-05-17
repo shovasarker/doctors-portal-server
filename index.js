ba8e67db283b5afe57fd46ad28b6e14294443b17
ShovaSarker <shovasarker01@gmail.com>
2022-05-18 02:49:02 +0600

    Mongodb Database basic setup complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb')
 require('dotenv').config()
 
 const app = express()
@@ -11,6 +12,22 @@ const port = process.env.PORT || 5000
 app.get('/', (req, res) => {
   res.send('Doctors Portal Server is Running')
 })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hbsug.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+})
+
+const run = async () => {
+  try {
+    await client.connect()
+  } finally {
+  }
+}
+
+run().catch(console.dir)
 
 app.listen(port, () =>
   console.log('doctors portal server is running on, ', port)
